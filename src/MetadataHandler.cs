b735bb216cef567253a2b15733fb68f2095fd6fe
kejones8 <54409533+kejones8@users.noreply.github.com>
2022-12-16 10:46:50 -0500

    Add files via upload

---
+++ src/MetadataHandler.cs
@@ -0,0 +1,117 @@
+﻿//  Authors:  Robert M. Scheller, Alec Kretchun, Vincent Schuster
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Landis.Library.Metadata;
+using Landis.Utilities;
+using Landis.Core;
+
+namespace Landis.Extension.Scrapple
+{
+    public static class MetadataHandler
+    {
+        
+        public static ExtensionMetadata Extension {get; set;}
+
+        public static void InitializeMetadata(int Timestep, ICore mCore)
+        {
+            ScenarioReplicationMetadata scenRep = new ScenarioReplicationMetadata() {
+                RasterOutCellArea = PlugIn.ModelCore.CellArea,
+                TimeMin = PlugIn.ModelCore.StartTime,
+                TimeMax = PlugIn.ModelCore.EndTime
+            };
+
+            Extension = new ExtensionMetadata(mCore){
+                Name = PlugIn.ExtensionName,
+                TimeInterval = Timestep, 
+                ScenarioReplicationMetadata = scenRep
+            };
+
+            //---------------------------------------
+            //          table outputs:   
+            //---------------------------------------
+
+            PlugIn.ignitionsLog = new MetadataTable<IgnitionsLog>("scrapple-ignitions-log.csv");
+
+            OutputMetadata tblOut_igns = new OutputMetadata()
+            {
+                Type = OutputType.Table,
+                Name = "ClimateFireIgnitionsLog",
+                FilePath = PlugIn.ignitionsLog.FilePath,
+                Visualize = false,
+            };
+            tblOut_igns.RetriveFields(typeof(IgnitionsLog));
+            Extension.OutputMetadatas.Add(tblOut_igns);
+
+            PlugIn.eventLog = new MetadataTable<EventsLog>("scrapple-events-log.csv");
+
+            OutputMetadata tblOut_events = new OutputMetadata()
+            {
+                Type = OutputType.Table,
+                Name = "ClimateFireEventsLog",
+                FilePath = PlugIn.eventLog.FilePath,
+                Visualize = false,
+            };
+            tblOut_events.RetriveFields(typeof(EventsLog));
+            Extension.OutputMetadatas.Add(tblOut_events);
+
+            PlugIn.summaryLog = new MetadataTable<SummaryLog>("scrapple-summary-log.csv");
+
+            OutputMetadata tblSummaryOut_events = new OutputMetadata()
+            {
+                Type = OutputType.Table,
+                Name = "ClimateFireSummaryLog",
+                FilePath = PlugIn.summaryLog.FilePath,
+                Visualize = false,
+            };
+            tblSummaryOut_events.RetriveFields(typeof(SummaryLog));
+            Extension.OutputMetadatas.Add(tblSummaryOut_events);
+
+            //---------------------------------------            
+            //          map outputs:         
+            //---------------------------------------
+            string intensityMapFileName = "scrapple-intensity.img";
+            OutputMetadata mapOut_Intensity = new OutputMetadata()
+            {
+                Type = OutputType.Map,
+                Name = "Intensity",
+                FilePath = @intensityMapFileName,
+                Map_DataType = MapDataType.Ordinal,
+                Map_Unit = FieldUnits.Severity_Rank,
+                Visualize = true,
+            };
+            Extension.OutputMetadatas.Add(mapOut_Intensity);
+
+            //string specialDeadWoodMapFileName = "scrapple-dead-wood.img";
+            //OutputMetadata mapOut_SpecialDead = new OutputMetadata()
+            //{
+            //    Type = OutputType.Map,
+            //    Name = "SpecialDeadWood",
+            //    FilePath = @specialDeadWoodMapFileName,
+            //    Map_DataType = MapDataType.Continuous,
+            //    Map_Unit = FieldUnits.g_C_m2,
+            //    Visualize = true,
+            //};
+            //Extension.OutputMetadatas.Add(mapOut_Intensity);
+            //OutputMetadata mapOut_Time = new OutputMetadata()
+            //{
+            //    Type = OutputType.Map,
+            //    Name = "TimeLastFire",
+            //    FilePath = @TimeMapFileName,
+            //    Map_DataType = MapDataType.Continuous,
+            //    Map_Unit = FieldUnits.Year,
+            //    Visualize = true,
+            //};
+            //Extension.OutputMetadatas.Add(mapOut_Time);
+            //---------------------------------------
+            MetadataProvider mp = new MetadataProvider(Extension);
+            mp.WriteMetadataToXMLFile("Metadata", Extension.Name, Extension.Name);
+
+
+
+
+        }
+    }
+}
---
+++ src/SummaryLog.cs
@@ -0,0 +1,60 @@
+﻿//  Authors:  Robert M. Scheller, Alec Kretchun, Vincent Schuster
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Landis.Library.Metadata;
+
+namespace Landis.Extension.Scrapple
+{
+    public class SummaryLog
+    {
+
+        [DataFieldAttribute(Unit = FieldUnits.Year, Desc = "Simulation Year")]
+        public int SimulationYear {set; get;}
+
+        [DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Total Sites Accidental Burned")]
+        public int TotalBurnedSitesAccidental { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Total Sites Lightning Burned")]
+        public int TotalBurnedSitesLightning { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Total Sites Rx Burned")]
+        public int TotalBurnedSitesRx { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Accidental Fires")]
+        public int NumberFiresAccidental { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Lightning Fires")]
+        public int NumberFiresLightning { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Rx Fires")]
+        public int NumberFiresRx { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.g_B_m2, Desc = "Biomass Killed Accidental")]
+        public int TotalBiomassMortalityAccidental { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.g_B_m2, Desc = "Biomass Killed Lightning")]
+        public int TotalBiomassMortalityLightning { set; get; }
+
+        [DataFieldAttribute(Unit = FieldUnits.g_B_m2, Desc = "Biomass Killed Rx")]
+        public int TotalBiomassMortalityRx { set; get; }
+
+        //[DataFieldAttribute(Desc = "Average dNBR")]
+        //public int AverageDNBR { set; get; }
+        //[DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Cells Intensity 1")]
+        //public double NumberCellsSeverity1 { set; get; }
+
+        //[DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Cells Intensity 2")]
+        //public double NumberCellsSeverity2 { set; get; }
+
+        //[DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Cells Intensity 3")]
+        //public double NumberCellsSeverity3 { set; get; }
+
+        //[DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Cells Intensity 4")]
+        //public double NumberCellsSeverity4 { set; get; }
+        //[DataFieldAttribute(Unit = FieldUnits.Count, Desc = "Number of Cells Intensity 5")]
+        //public double NumberCellsSeverity5 { set; get; }
+    }
+}
